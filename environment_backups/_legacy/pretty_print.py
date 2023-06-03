9d5dfbc32d9c94c137245b52b141c63e5ce18dae
Luis C. Berrocal <luis.berrocal@payjoy.com>
2023-06-03 08:16:57 -0500

    Arg parser working with backups

---
+++ environment_backups/_legacy/pretty_print.py
@@ -0,0 +1,10 @@
+def print_success(message):
+    print(f"\033[92m {message}\033[00m")
+
+
+def print_warning(message):
+    print(f"\033[93m {message}\033[00m")
+
+
+def print_error(message):
+    print(f"\033[91m {message}\033[00m")
