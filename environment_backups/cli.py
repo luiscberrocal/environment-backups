42f2c71f1740ea0d57578f70989c501031853f9b
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-06-03 10:57:54 -0500

    CLI with argparses

--- environment_backups/cli.py
+++ environment_backups/cli.py
@@ -1,4 +1,5 @@
 """Console script for environment_backups."""
+import argparse
 
 import click
 
@@ -11,8 +12,33 @@ def main():
     click.echo("CLI Application to backup environment variables.")
 
 
+def main_arg_parser(parsed_args):
+    print(type(parsed_args))
+    print(parsed_args)
+
+
 if __name__ == "__main__":
     """
     backup-environments -f /project/folders -t /target_folder/ -e .envs -e .env
     """
-    main()  # pragma: no cover
+    parser = argparse.ArgumentParser(description="Environment backups")
+    parser.add_argument(
+        "-e", "--environment-folder",
+        type=str,
+        nargs="+",
+        help="Name of the environment folder to backup"
+    )
+
+    parser.add_argument(
+        "-f", "--folder",
+        type=str,
+        help="Folder where the projects are found."
+    )
+    parser.add_argument(
+        "-t", "--target-folder",
+        type=str,
+        help="Folder to save the zipped files"
+    )
+    parsed_args = parser.parse_args()
+    # main()  # pragma: no cover
+    main_arg_parser(parsed_args)
