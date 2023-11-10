c127346b5d1fe56eecdbb6c55872dd090ccbb12f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-10 18:37:07 -0500

    About is working.

--- environment_backups/__init__.py
+++ environment_backups/__init__.py
@@ -4,6 +4,9 @@ __author__ = """Luis C. Berrocal"""
 __email__ = 'luis.berrocal.1942@gmail.com'
 __version__ = '0.3.0'
 
+from rich.console import Console
 from environment_backups.config.configuration import ConfigurationManager
 
 CONFIGURATION_MANAGER = ConfigurationManager()
+
+CONSOLE = Console()
--- environment_backups/cli.py
+++ environment_backups/cli.py
@@ -6,9 +6,10 @@ from typing import List
 import click
 from rich.panel import Panel
 
-from environment_backups import CONFIGURATION_MANAGER
-from environment_backups.backups.cli_commands import backup
+from environment_backups import CONFIGURATION_MANAGER, CONSOLE
 from environment_backups import __version__ as current_version
+from environment_backups.backups.cli_commands import backup
+
 
 @click.group()
 def main():
@@ -16,6 +17,8 @@ def main():
     click.echo("environment-backups")
     click.echo("=" * len("environment-backups"))
     click.echo("CLI Application to backup environment variables.")
+
+
 @click.command()
 def about():
     app_name = CONFIGURATION_MANAGER.APP_NAME.replace('-', ' ').title()
@@ -26,6 +29,7 @@ def about():
     panel = Panel('\n'.join(content), title=app_name, subtitle=f"version: {current_version}")
     CONSOLE.print(panel)
 
+
 if __name__ == "__main__":
     """
     environment-backups -p /project/folders -b /target_folder/
@@ -33,3 +37,5 @@ if __name__ == "__main__":
     # main()  # pragma: no cover
 
 main.add_command(backup)
+main.add_command(about)
+
