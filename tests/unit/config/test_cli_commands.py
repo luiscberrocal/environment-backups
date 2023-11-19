e4609a8dc6fac6060ee72a249f91b478a32c904d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-19 10:57:59 -0500

    Init tests working.

--- tests/unit/config/test_cli_commands.py
+++ tests/unit/config/test_cli_commands.py
@@ -20,8 +20,8 @@ def test_init_command(mock_config_manager, tmp_path):
     mock_config_manager.get_current.return_value = {}
     runner = CliRunner()
     mock_inputs = '\n'.join(['%Y-%m-%d', '.envs', '',
-                             'my_config_name', str(tmp_path), str(tmp_path), "my_computer_name"
-                                                                             'N', 'Y'])
+                             'my_config_name', str(tmp_path), str(tmp_path), "my_computer_name",
+                                                                             'N', 'N', 'y'])
     result = runner.invoke(config, ['init'], input=mock_inputs)
 
     assert result.exit_code == 0
@@ -30,3 +30,16 @@ def test_init_command(mock_config_manager, tmp_path):
     mock_config_manager.set_configuration.assert_called_once()
     if "Yes" in mock_inputs.split('\n'):
         mock_config_manager.save.assert_called_once()
+
+
+def test_reset_delete(mock_config_manager):
+    mock_config_manager.get_current.return_value = {}
+    runner = CliRunner()
+    mock_inputs = '\n'.join(['Y'])
+    result = runner.invoke(config, ['reset'], input=mock_inputs)
+
+    assert result.exit_code == 0
+    lines = result.output.split('\n')
+    assert len(lines) == 3
+    assert 'By resetting the configuration the' in lines[0]
+    assert 'Configuration file deleted. A backup was created ' in lines[1]
