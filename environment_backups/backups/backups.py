26e1c7c469f8f7650d07c9b856b1e966999ee5ec
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-21 09:35:27 -0500

    Backup by name function.

--- environment_backups/backups/backups.py
+++ environment_backups/backups/backups.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from pathlib import Path
 from typing import List, Dict, Any, Tuple
 
-from deprecated import deprecated
 from pyzipper import AESZipFile, ZIP_DEFLATED, WZ_AES
 
 from environment_backups import CONFIGURATION_MANAGER
@@ -54,11 +53,15 @@ def zip_folder_with_pwd(zip_file: Path, folder_to_zip: Path, password: str = Non
             zf.setpassword(pwd)
         zipdir(folder_to_zip, zf)
 
+
 # TODO Add * to force use of names.
-def backup_envs(projects_folder: Path, backup_folder: Path,
-                environment_folders: List[str], password: str = None,
+def backup_envs(projects_folder: Path,
+                backup_folder: Path,
+                environment_folders: List[str],
+                password: str = None,
                 date_format='%Y%m%d_%H', ) -> Tuple[List[Path], Path]:
     project_envs_dict = get_projects_envs(projects_folder, environment_folders)
+    # TODO add computer name to the folder?? or the file??
     timestamp = datetime.now().strftime(date_format)
     b_folder = backup_folder / timestamp
     b_folder.mkdir(exist_ok=True)
@@ -70,13 +73,25 @@ def backup_envs(projects_folder: Path, backup_folder: Path,
     return zip_list, b_folder
 
 
-def backup_environments(environment_name: str):
+def backup_environments(environment_name: str) -> Tuple[List[Path], Path]:
     app_configuration = CONFIGURATION_MANAGER.get_current()
     cfg, _ = get_configuration_by_name(environment_name, app_configuration)
     if cfg is None:
         error_message = f'No environment configuration found for "{environment_name}"'
         raise ConfigurationError(error_message)
+    pwd = CONFIGURATION_MANAGER.get_current().get('password')
+    if len(pwd) == 0:
+        pwd = None
+
+    environment_folders = app_configuration.get('env_folder_pattern')
+    date_format = app_configuration.get('date_format')
     project_folder = Path(cfg['project_folder'])
     backup_folder = Path(cfg['backup_folder'])
-
-
+    zip_list, b_folder = backup_envs(
+        projects_folder=project_folder,
+        backup_folder=backup_folder,
+        environment_folders=environment_folders,
+        password=pwd,
+        date_format=date_format
+    )
+    return zip_list, b_folder
