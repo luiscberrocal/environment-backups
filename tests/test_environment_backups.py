e38669a024c557f5575c7efea2521486a65d574f
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-24 06:07:03 -0500

    Fixing tests

--- tests/test_environment_backups.py
+++ tests/test_environment_backups.py
@@ -1,35 +1,50 @@
 #!/usr/bin/env python
 """Tests for `environment_backups` package."""
 
-import pytest
 from click.testing import CliRunner
 
 from environment_backups import cli
-
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-    del response
 
 
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
     result = runner.invoke(cli.main)
+    result_lines = result.output.split('\n')
+    expected_lines = [
+        "Usage: main [OPTIONS] COMMAND [ARGS]...",
+        "",
+        "  Main entrypoint.",
+        "",
+        "Options:",
+        "  --help  Show this message and exit.",
+        "",
+        "Commands:",
+        "  about",
+        "  backup",
+        "  config  Configuration entrypoint.",
+        ""
+    ]
+    assert len(result_lines) == len(expected_lines)
     assert result.exit_code == 0
-    assert 'environment-backups' in result.output
+    for i, line in enumerate(result_lines):
+        assert line == expected_lines[i]
+
+
+def test_help():
+    runner = CliRunner()
     help_result = runner.invoke(cli.main, ['--help'])
+
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    output_lines = help_result.output.split('\n')
+
+    # ------------------------------
+    var_name = 'output_lines'
+    var_value = eval(var_name)
+    from pathlib import Path
+    import json
+    file = Path(__name__) / f'{var_name}.json'
+    with open(file, 'w') as f:
+        json.dump(var_value, f, indent=4)
+    print(f'Saved file')
+    # ------------------------------------
