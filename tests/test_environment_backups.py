76bfbd502bf44880df8da7088b470834e3ba700e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-24 06:10:34 -0500

    Added test for help.

--- tests/test_environment_backups.py
+++ tests/test_environment_backups.py
@@ -33,18 +33,26 @@ def test_command_line_interface():
 
 def test_help():
     runner = CliRunner()
-    help_result = runner.invoke(cli.main, ['--help'])
+    result = runner.invoke(cli.main, ['--help'])
 
-    assert help_result.exit_code == 0
-    output_lines = help_result.output.split('\n')
+    assert result.exit_code == 0
+    result_lines = result.output.split('\n')
 
-    # ------------------------------
-    var_name = 'output_lines'
-    var_value = eval(var_name)
-    from pathlib import Path
-    import json
-    file = Path(__name__) / f'{var_name}.json'
-    with open(file, 'w') as f:
-        json.dump(var_value, f, indent=4)
-    print(f'Saved file')
-    # ------------------------------------
+    expected_lines = [
+        "Usage: main [OPTIONS] COMMAND [ARGS]...",
+        "",
+        "  Main entrypoint.",
+        "",
+        "Options:",
+        "  --help  Show this message and exit.",
+        "",
+        "Commands:",
+        "  about",
+        "  backup",
+        "  config  Configuration entrypoint.",
+        ""
+    ]
+    assert len(result_lines) == len(expected_lines)
+    assert result.exit_code == 0
+    for i, line in enumerate(result_lines):
+        assert line == expected_lines[i]
