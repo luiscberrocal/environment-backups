e364176c30077c426d21e185e3c054190524f396
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-28 06:36:29 -0500

    Folder creation tool testing and improvement.

--- tests/tools.py
+++ tests/tools.py
@@ -1,21 +1,31 @@
+import shutil
 from pathlib import Path
-from typing import List
+from typing import List, Tuple
 
 
-def create_projects_folder(*, root_folder: Path,
-                           projects_folder_name: str = 'MyProjectsForTests',
-                           projects_folders: List[str] = None,
-                           env_folders: List[str]= ['.envs']):
+def create_projects_folder_for_tests(*, root_folder: Path,
+                                     projects_folder_name: str = 'MyProjectsForTests',
+                                     projects_folders: List[str] = None,
+                                     env_folders: List[str] = None,
+                                     env_file_count: int = 1) -> Tuple[Path, List[Path]]:
     if projects_folders is None:
         projects_folders = ['project1']
+    if env_folders is None:
+        env_folders = ['.envs']
+
     projects_folder = root_folder / projects_folder_name
     projects_folder.mkdir()
-
+    environment_files = []
     for folder_name in projects_folders:
         project_folder = projects_folder / folder_name
         project_folder.mkdir()
         for env_name in env_folders:
             env_folder = project_folder / env_name
             env_folder.mkdir()
-            config_file = env_folder / 'dummy.txt'
-            config_file.touch()
+            for i in range(env_file_count):
+                config_file = env_folder / f'dummy{i}.txt'
+                config_file.touch()
+                environment_files.append(config_file)
+    return projects_folder, environment_files
+
+    # shutil.rmtree(project_folder)
--- tests/unit/test_tools.py
+++ tests/unit/test_tools.py
@@ -1,6 +1,7 @@
+import shutil
 from pathlib import Path
 
-from tests.tools import create_projects_folder
+from tests.tools import create_projects_folder_for_tests
 
 
 def test_create_projects_folder():
@@ -8,10 +9,15 @@ def test_create_projects_folder():
     root_folder = Path(__file__).parent.parent.parent / 'output'
     assert root_folder.exists()
     projects_list = ['project1', 'project2']
-    create_projects_folder(root_folder=root_folder, projects_folders=projects_list)
+    projects_folder, config_files = create_projects_folder_for_tests(root_folder=root_folder,
+                                                                     projects_folders=projects_list)
 
     projects_folder = root_folder / 'MyProjectsForTests'
     assert projects_folder.exists()
     for project in projects_list:
         assert (projects_folder / project).exists()
-        # TODO check file env content
+
+    for config_file in config_files:
+        assert config_file.exists()
+    shutil.rmtree(projects_folder)
+
