b329848cf3c0d529bcf6f011cde2cbd1301a56e8
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-11-30 06:27:16 -0500

    Fixed edit test

--- tests/unit/config/test_edit_command.py
+++ tests/unit/config/test_edit_command.py
@@ -7,7 +7,7 @@ from environment_backups.config.cli_commands import edit
 from tests.factories import configuration_factory
 
 
-def test_edit_command(tmp_path):
+def test_edit_command_no_changes(tmp_path):
     """
     Test the 'edit' command of the CLI application.
 
@@ -18,10 +18,10 @@ def test_edit_command(tmp_path):
     """
     # Mock CONFIGURATION_MANAGER
     mock_config_manager = MagicMock()
-    
+
     projects_folder = tmp_path / 'MyProjects'
     app_configuration = configuration_factory(projects_folder=projects_folder, google_support=False)
-    app_configuration_dict = app_configuration.model_dump(mode='json')
+    app_configuration_dict = app_configuration.model_dump()
     mock_config_manager.get_current.return_value = app_configuration_dict
 
     mock_config_manager.config_file = '/path/to/config.toml'
@@ -35,9 +35,27 @@ def test_edit_command(tmp_path):
         result = runner.invoke(edit, input=mock_inputs)
 
     # Assertions
+    output_lines = result.output.split('\n')
+    expected_lines = [
+        "Init configuration file: /path/to/config.toml",
+        "Date format for backup folder prefix [%Y%m%d_%H]: YYYY-MM-DD",
+        "Environment folder pattern name to parse. If several separate by a comma [.envs]: pattern1, pattern3",
+        "Default password for zip files: new_password",
+        "Do you want to edit the configuration for test_config_0 [y/N]: y",
+        "Name of the configuration. Must be unique [test_config_0]: ",
+        f"Project folder [{app_configuration.configurations[0].projects_folder}]: ",
+        f"Backup folder [{app_configuration.configurations[0].backup_folder}]: ",
+        "Computer name [deep-space9_0]: ",
+        "Save configuration? [y/N]: y",
+        ""
+    ]
     # assert 'Init configuration file: /path/to/config.toml' in result.output
+    assert len(output_lines) == 11
     assert result.exit_code == 0
+    for i, line in enumerate(output_lines):
+        assert line == expected_lines[i]
     mock_config_manager.set_configuration.assert_called_once()
+    # TODO assert called one with for set_configuration
     mock_config_manager.save.assert_called_once()
 
 
