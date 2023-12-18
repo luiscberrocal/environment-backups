43f1a017726a80a07fea2c5187bceede863741e8
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-12-18 08:22:32 -0500

    Zipping not wokring yet.

--- environment_backups/_experimental/zipper.py
+++ environment_backups/_experimental/zipper.py
@@ -1,39 +1,60 @@
 import asyncio
 import os
-import zipfile
+import time
 from pathlib import Path
+from typing import List
+
+import pyzipper
 
 
-async def async_zip_folder(source_folder: Path, zip_file: Path):
+async def zip_folder_with_pwd_async(folder: Path, zip_file: Path, password: str = None):
     """
-    Asynchronously compresses the contents of a folder into a zip file.
-
-    :param source_folder: Path to the source folder to be zipped.
-    :param zip_file: Path to the output zip file.
+    Asynchronously compresses a single folder into a zip file with optional password protection.
     """
     loop = asyncio.get_running_loop()
-    await loop.run_in_executor(None, sync_zip_folder, source_folder, zip_file)
+    await loop.run_in_executor(
+        None,
+        sync_zip_folder_with_pwd,
+        folder,
+        zip_file,
+        password
+    )
 
 
-def sync_zip_folder(source_folder: Path, zip_file: Path):
+def sync_zip_folder_with_pwd(folder: Path, zip_file: Path, password: str = None):
     """
-    Synchronously compresses the contents of a folder into a zip file.
-
-    :param source_folder: Path to the source folder to be zipped.
-    :param zip_file: Path to the output zip file.
+    Synchronously compresses a single folder into a zip file with optional password protection.
     """
-    with zipfile.ZipFile(zip_file, 'w', zipfile.ZIP_DEFLATED) as zipf:
-        for root, dirs, files in os.walk(source_folder):
+    with pyzipper.AESZipFile(zip_file, 'w', compression=pyzipper.ZIP_DEFLATED, strict_timestamps=False) as zipf:
+        if password:
+            zipf.setpassword(password.encode('utf-8'))
+        for root, dirs, files in os.walk(folder):
             for file in files:
                 file_path = Path(root) / file
-                zipf.write(file_path, file_path.relative_to(source_folder))
+                zipf.write(file_path, file_path.relative_to(folder.parent))
+
+
+async def zip_folders_with_pwd(source_folder: Path, backup_folder: Path, password: str = None) -> List[Path]:
+    zipped_files = []
+    for item in source_folder.iterdir():
+        if item.is_dir():
+            zip_file_path = backup_folder / f"{item.name}.zip"
+            print(f'Zipping {item.name} to {zip_file_path}')
+            await zip_folder_with_pwd_async(item, zip_file_path, password)
+            zipped_files.append(zip_file_path)
+    return zipped_files
 
 
 # Example usage
 async def main():
-    my_downloads = Path.home() / 'Downloads'
-    zz_file = Path.home() / 'Documents' / '__zipping_test' / 'tmep.zip'
-    await async_zip_folder(my_downloads, zz_file)
+    source = Path.home() / 'Downloads'
+    backup = Path.home() / 'Documents' / '__zipping_test'
+
+    zipped_files = await zip_folders_with_pwd(source, backup, None)
+    print("Zipped files:", zipped_files)
+
 
 if __name__ == '__main__':
+    start = time.time()
     asyncio.run(main())
+    print(f'Elapsed: {(time.time() - start):.2f} seconds.')
