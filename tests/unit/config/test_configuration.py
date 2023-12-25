58fe58d7ec9ffbabf2be8d1cc4b22d889a71d7a2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2023-12-25 10:29:15 -0500

    Fixing tests.

--- tests/unit/config/test_configuration.py
+++ tests/unit/config/test_configuration.py
@@ -7,21 +7,21 @@ from environment_backups.config.configuration import ConfigurationManager
 
 
 class TestConfigurationManager:
-    def test_init(self, tmp_config_folder):
-        configuration = ConfigurationManager(tmp_config_folder)
+    def test_init(self, tmp_path):
+        configuration = ConfigurationManager(tmp_path)
         assert not configuration.config_file.exists()
 
-    def test_import_from_json(self, tmp_config_folder, fixtures_folder):
+    def test_import_from_json(self, tmp_path, fixtures_folder):
         config_json_file = fixtures_folder / 'app_configuration.json'
 
-        configuration = ConfigurationManager(tmp_config_folder)
+        configuration = ConfigurationManager(tmp_path)
 
         config_data = configuration.import_from_json(config_json_file).get_current()
 
         assert configuration.config_file.exists()
         assert config_data
         assert config_data['application']
-        assert len(config_data['configurations'])
+        assert len(config_data['configurations']) == 2
 
 
 def test_initialization_with_default_path():
